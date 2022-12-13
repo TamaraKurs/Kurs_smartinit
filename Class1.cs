f78c8251067de8c4cf3e4e8f106126348775049c
SMART21\SmartInIT <tamara.mitrovic@panonit.com>
2022-12-13 13:31:10 +0100

    C# project init.

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject1
+{
+    class LoginPage
+    {
+        IWebDriver driver = new ChromeDriver();
+        
+        [Test]
+        public void Login()
+        {
+            driver.Navigate().GoToUrl("https://elearn.smartinit.net/login/index.php");
+            driver.Manage().Window.Maximize();
+
+        }
+    }
+}
