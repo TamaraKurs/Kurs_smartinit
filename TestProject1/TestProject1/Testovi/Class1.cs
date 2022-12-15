2130b2941b3642d44791dbc1afb2124be62802f3
SMART21\SmartInIT <tamara.mitrovic@panonit.com>
2022-12-15 14:40:42 +0100

    no message

--- TestProject1/TestProject1/Testovi/Class1.cs
+++ TestProject1/TestProject1/Testovi/Class1.cs
@@ -9,6 +9,7 @@ namespace TestProject1
 {
     class LoginPage
     {
+        #region Testovi
         IWebDriver driver = new ChromeDriver();
 
         [Test]
@@ -17,6 +18,36 @@ namespace TestProject1
             driver.Navigate().GoToUrl("https://elearn.smartinit.net/login/index.php");
             driver.Manage().Window.Maximize();
 
+            var korisnickoIme = driver.FindElement(By.Id("username"));
+            Assert.NotNull(korisnickoIme);
+
+            var lozinka = driver.FindElement(By.Id("password"));
+            Assert.NotNull(lozinka);
+
+            var dugme = driver.FindElement(By.Id("loginbtn"));
+            Assert.NotNull(dugme);
+
+
+        }
+        [Test]
+        public void Login_2()
+        {
+            driver.Navigate().GoToUrl("https://elearn.smartinit.net/login/index.php");
+            driver.Manage().Window.Maximize();
+
+
+            driver.FindElement(By.Id("username")).SendKeys("tamara.mitrovic");
+
+            driver.FindElement(By.Id("password")).SendKeys("tamara.mitrovic");
+
+            driver.FindElement(By.Id("loginbtn")).Click();
+
+            var stranica = driver.FindElement(By.XPath("/html/body/header/nav/div/div/div/div/div[1]/div[1]/ul/li/a"));
+            Assert.NotNull(stranica);
+
+
+
         }
     }
+    #endregion
 }
