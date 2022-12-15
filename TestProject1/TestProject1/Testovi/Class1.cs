e608457ac65d352aa7b1bf4f7c5a8ff175374033
SMART21\SmartInIT <tamara.mitrovic@panonit.com>
2022-12-15 18:09:15 +0100

    aaa

--- TestProject1/TestProject1/Testovi/Class1.cs
+++ TestProject1/TestProject1/Testovi/Class1.cs
@@ -44,9 +44,68 @@ namespace TestProject1
 
             var stranica = driver.FindElement(By.XPath("//*[contains(text(), 'Moji kursevi')]"));
             Assert.NotNull(stranica);
-            
+
 
         }
-    }
+
+        [Test]
+        public void Login_3()
+        {
+            driver.Navigate().GoToUrl("https://elearn.smartinit.net/login/index.php");
+            driver.Manage().Window.Maximize();
+
+
+            driver.FindElement(By.Id("username")).SendKeys("ffafads");
+
+            driver.FindElement(By.Id("password")).SendKeys("kfdfasd");
+
+            driver.FindElement(By.Id("loginbtn")).Click();
+
+            var stranica = driver.FindElement(By.XPath("//*[contains(text(), 'Pogrešno korisničko ime ili lozinka. Molimo pokušajte ponovo.')]"));
+            Assert.NotNull(stranica);
+
+
+        }
+        [Test]
+        public void Login_4()
+        {
+            driver.Navigate().GoToUrl("https://elearn.smartinit.net/login/index.php");
+            driver.Manage().Window.Maximize();
+
+
+            driver.FindElement(By.Id("username")).SendKeys("nenad.antonic@live.com");
+
+            driver.FindElement(By.Id("password")).SendKeys("klasikaK1-");
+
+            driver.FindElement(By.Id("loginbtn")).Click();
+
+            var stranica = driver.FindElement(By.XPath("//*[contains(text(), 'Automatsko testiranje softvera 4/22')]"));
+            Assert.NotNull(stranica);
+        }
+            [Test]
+            public void Login_5()
+            {
+                driver.Navigate().GoToUrl("https://elearn.smartinit.net/login/index.php");
+                driver.Manage().Window.Maximize();
+
+
+                driver.FindElement(By.Id("username")).SendKeys("nenad.antonic@live.com");
+
+                driver.FindElement(By.Id("password")).SendKeys("klasikaK1-");
+
+                driver.FindElement(By.Id("loginbtn")).Click();
+
+                driver.FindElement(By.XPath("//a[contains(text(), 'Automatsko testiranje softvera 4/22')]")).Click();
+
+                var pythonUvod = driver.FindElement(By.XPath("//*[contains(text(), 'Python Uvod')]"));
+                Assert.NotNull(pythonUvod);
+
+                var resenjaZadataka = driver.FindElement(By.XPath("//*[contains(text(), 'Rešenja domaćih zadataka')]"));
+               Assert.NotNull(resenjaZadataka);
+
+
+        }
+        }
     #endregion
+
 }
